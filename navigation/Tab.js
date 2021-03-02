266675ed5fa241a8b02de3cd52524ff3220313e2
leeedev <leeesangheee@gmail.com>
2021-03-02 21:28:37 +0900

    Update tab title

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Movie from "../screens/Movie";
@@ -8,12 +8,22 @@ import Like from "../screens/Like";
 
 const Tab = createBottomTabNavigator();
 
-export default () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Movie" component={Movie} />
-    <Tab.Screen name="TV" component={TV} />
-    <Tab.Screen name="Search" component={Search} />
-    <Tab.Screen name="Like" component={Like} />
-  </Tab.Navigator>
-);
+export default ({ navigation: { setOptions }, route: { state } }) => {
+  useLayoutEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      setOptions({ title: state?.routeNames[state.index] || "Home" });
+    }
+    return () => (mounted = false);
+  }, [state]);
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Movie" component={Movie} />
+      <Tab.Screen name="TV" component={TV} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Like" component={Like} />
+    </Tab.Navigator>
+  );
+};
