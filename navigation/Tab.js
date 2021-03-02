311a2528b7c10a59b07fc65c4835b518a15bc5b0
leeedev <leeesangheee@gmail.com>
2021-03-02 21:05:30 +0900

    Tab navigation

---
+++ navigation/Tab.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Movie from "../screens/Movie";
+import TV from "../screens/TV";
+import Search from "../screens/Search";
+import Like from "../screens/Like";
+
+const Tab = createBottomTabNavigator();
+
+export default () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Movie" component={Movie} />
+    <Tab.Screen name="TV" component={TV} />
+    <Tab.Screen name="Search" component={Search} />
+    <Tab.Screen name="Like" component={Like} />
+  </Tab.Navigator>
+);
