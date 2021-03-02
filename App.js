589bbae81870acbf4368da169bf28b6504ce4e6c
leeedev <leeesangheee@gmail.com>
2021-03-02 17:43:46 +0900

    Cache image, icon, font

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { View, Text } from "react-native";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text, Image } from "react-native";
+
+const cacheImages = (images) =>
+  images.map((image) =>
+    typeof image === "string"
+      ? Image.prefetch(image)
+      : Asset.fromModule(image).downloadAsync()
+  );
+const cacheIcons = (icons) => icons.map((icon) => Font.loadAsync(icon));
+const cacheFonts = () => {
+  Font.loadAsync({
+    "ObjectSans-Regular": require("./assets/fonts/ObjectSans/ObjectSans-Regular.otf"),
+    "ObjectSans-Slanted": require("./assets/fonts/ObjectSans/ObjectSans-Slanted.otf"),
+  });
+};
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
 
-  const startAsync = async () => {};
+  const startAsync = async () => {
+    const images = cacheImages([require("./assets/splash.png")]);
+    const icons = cacheIcons([Ionicons.font]);
+    const fonts = cacheFonts();
+    return Promise.all([...images, ...icons, ...fonts]);
+  };
   const onFinish = () => setIsReady(true);
 
   return (
