47ba56875be29924c7da8aa57b3677e374a48ed2
leeedev <leeesangheee@gmail.com>
2021-03-05 03:33:15 +0900

    Mark vote in color

--- components/Vote.js
+++ components/Vote.js
@@ -5,22 +5,34 @@ import PropTypes from "prop-types";
 
 const Container = styled.View`
   flex-direction: row;
+  align-items: center;
 `;
-const Title = styled.Text``;
 const HalfTitle = styled.Text`
   width: 9px;
   overflow: hidden;
 `;
-const Flower = styled.Text``;
+const Title = styled.Text``;
+const Flower = styled.Text`
+  font-size: 20px;
+`;
 
 const Vote = ({ vote }) => {
   return (
     <Container>
       <Title style={StyleSheet.Title}>{"❋".repeat(vote / 2)}</Title>
       {vote % 2 > 1 && (
-        <HalfTitle>
-          <Flower style={StyleSheet.Title}>❋</Flower>
-        </HalfTitle>
+        <>
+          {/* <HalfTitle>
+            <Flower style={StyleSheet.Title}>❋</Flower>
+          </HalfTitle> */}
+          <Flower
+            style={{
+              color: `rgba(0, 0, 0, ${(vote % 2) / 3})`,
+            }}
+          >
+            ❋
+          </Flower>
+        </>
       )}
     </Container>
   );
