55d2ea9a55922e7843edbe3337189c8b6bc94add
leeedev <leeesangheee@gmail.com>
2021-03-06 02:43:35 +0900

    Separate Slide from Slider

---
+++ components/Movie/Slide.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import StyleSheet from "../StyleSheet";
+import { Ionicons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+import { Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
+import Poster from "../Poster";
+import Vote from "../Vote";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const Container = styled.View`
+  margin-bottom: 16px;
+`;
+const Movie = styled.View`
+  width: ${WIDTH / 3}px;
+  margin-right: 16px;
+  margin-bottom: 16px;
+`;
+const PosterContainer = styled.View`
+  width: ${WIDTH / 3}px;
+  height: ${WIDTH / 2}px;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-bottom: 16px;
+`;
+const Info = styled.View`
+  width: 100%;
+`;
+const Text = styled.Text``;
+const LikeButton = styled.Text`
+  position: absolute;
+  left: ${WIDTH / 3 - 24}px;
+  bottom: 20px;
+`;
+
+const Slide = ({ title, posterUrl, vote }) => {
+  const navigation = useNavigation();
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <Container>
+      <Movie>
+        <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
+          <PosterContainer>
+            <Poster posterUrl={posterUrl} />
+          </PosterContainer>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setLiked((prev) => !prev)}>
+          <LikeButton>
+            <Ionicons
+              name={liked ? "heart" : "heart-outline"}
+              size={20}
+              color={liked ? "rgb(255, 0, 84)" : "rgb(190, 184, 184)"}
+            />
+          </LikeButton>
+        </TouchableOpacity>
+
+        <Info>
+          <Text style={StyleSheet.Text} numberOfLines={1}>
+            {title}
+          </Text>
+          <Vote vote={vote} />
+        </Info>
+      </Movie>
+    </Container>
+  );
+};
+
+export default Slide;
--- components/Movie/Slider.js
+++ components/Movie/Slider.js
@@ -1,12 +1,8 @@
 import React from "react";
 import styled from "styled-components/native";
 import StyleSheet from "../StyleSheet";
-import PropTypes from "prop-types";
 import { Dimensions } from "react-native";
-import Poster from "../Poster";
-import Vote from "../Vote";
-import { useNavigation } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native";
+import Slide from "./Slide";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 const Container = styled.View`
@@ -15,51 +11,25 @@ const Container = styled.View`
 const ScrollView = styled.ScrollView`
   width: 100%;
 `;
-const Movie = styled.View`
-  width: ${WIDTH / 3}px;
-  margin-right: 16px;
-  margin-bottom: 16px;
-`;
-const PosterContainer = styled.View`
-  width: ${WIDTH / 3}px;
-  height: ${WIDTH / 2}px;
-  border-radius: 8px;
-  overflow: hidden;
-  margin-bottom: 16px;
-`;
-const Info = styled.View`
-  width: 100%;
-`;
 const Title = styled.Text`
   margin-bottom: 8px;
 `;
 
-const Slide = ({ movieList, title = "Title" }) => {
-  const navigation = useNavigation();
-
+const Slider = ({ movieList, title = "Title" }) => {
   return (
     <Container>
       <Title style={StyleSheet.Title}>{title}</Title>
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
         {movieList.map((movie) => (
-          <Movie>
-            <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
-              <PosterContainer>
-                <Poster posterUrl={movie.poster_path} />
-              </PosterContainer>
-            </TouchableOpacity>
-
-            <Info>
-              <Title style={StyleSheet.Title} numberOfLines={1}>
-                {movie.title}
-              </Title>
-              <Vote vote={movie.vote_average} />
-            </Info>
-          </Movie>
+          <Slide
+            title={movie.title}
+            posterUrl={movie.poster_path}
+            vote={movie.vote_average}
+          />
         ))}
       </ScrollView>
     </Container>
   );
 };
 
-export default Slide;
+export default Slider;
