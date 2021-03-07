dbb7ac482ca33452a60534725905a68d85943d2a
leeedev <leeesangheee@gmail.com>
2021-03-08 03:25:35 +0900

    Backdrop image

---
+++ components/Backdrop.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+import { imageApi } from "../api";
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
+const Backdrop = ({ backdropUrl }) => {
+  return (
+    <Image
+      resizeMode="cover"
+      source={{ uri: imageApi(backdropUrl) }}
+      blurRadius={10}
+    />
+  );
+};
+
+Backdrop.propTypes = { backdropUrl: PropTypes.string.isRequired };
+
+export default Backdrop;
