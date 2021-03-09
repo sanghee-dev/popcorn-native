fe08e3b9289ad47067802d660c9d737d28ff6a9b
leeedev <leeesangheee@gmail.com>
2021-03-09 12:04:10 +0900

    Remove inActive prop from NoPoster

--- components/NoPoster.js
+++ components/NoPoster.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
-import { useFocusEffect } from "@react-navigation/native";
 import StyleSheet from "./StyleSheet";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -13,27 +12,16 @@ const Container = styled.View`
 const NoPoster = ({ index }) => {
   const [isActive, setIsActive] = useState(false);
 
-  useFocusEffect(
-    React.useCallback(() => {
-      setIsActive(true);
-      return () => {
-        setIsActive(false);
-      };
-    }, [])
-  );
-
   return (
     <Container>
-      {isActive && (
-        <LinearGradient
-          style={StyleSheet.Gradient}
-          colors={
-            index % 2 === 0
-              ? ["rgba(0, 255, 84, 0)", "rgba(0, 255, 84, 1)"]
-              : ["rgba(0, 255, 84, 1)", "rgba(0, 255, 84, 0)"]
-          }
-        />
-      )}
+      <LinearGradient
+        style={StyleSheet.Gradient}
+        colors={
+          index % 2 === 0
+            ? ["rgba(0, 255, 84, 0)", "rgba(0, 255, 84, 1)"]
+            : ["rgba(0, 255, 84, 1)", "rgba(0, 255, 84, 0)"]
+        }
+      />
     </Container>
   );
 };
